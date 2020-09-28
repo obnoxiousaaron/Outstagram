5e4da470d489329e5d53fbcac41b8a08be21e435
Avery Lee <aaronmarcus777@gmail.com>
2020-09-28 17:34:03 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
 import UserProfilePage from "./pages/UserProfilePage";
+import video from "./video.mp4";
 import "./App.css"
 // import Modal from "./components/Modal"
 
@@ -10,6 +11,20 @@ function App() {
   return (
     <div className="divdiv">
       <Navbar />
+      <video
+        autoPlay
+        loop
+        muted
+        style={{
+          objectFit:"cover",
+          width:"100vw",
+          height:"100vh",
+          position: "fixed",
+          zIndex:"0"
+        }}
+        >
+          <source src={video} type="video/mp4"/>
+        </video>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/users/:id" component={UserProfilePage} />      
